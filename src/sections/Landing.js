3ae09f5f21b9db2a5629ee69838b9d23eda0c085
debase-v87 <74028234+debase-v87@users.noreply.github.com>
2020-11-08 14:59:45 -0600

    fix 3

--- src/sections/Landing.js
+++ src/sections/Landing.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export default function Landing({ scrollToOverview }) {
 	const content = (
 		<div className="container has-text-centered">
-			<h1 className="title is-size-1-tablet is-size-2-mobile">Debaseonomics</h1> 
-			<h2 className="subtitle is-size-3-tablet is-size-4-mobile">Boundless Experimentation</h2>
+			<h1 className="title is-size-1-tablet is-size-2-mobile">Debaseonomics v87</h1> 
+			<h2 className="subtitle is-size-3-tablet is-size-4-mobile">Boundless Flexibility</h2>
 			<div className="buttons has-addons is-centered">
 				<button onClick={() => scrollToOverview()} className="button is-rounded is-info">
 					Info
