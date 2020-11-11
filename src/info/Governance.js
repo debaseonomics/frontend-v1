2a3e6d44e83870cf09dbdcdc40c0353f55951b32
debase-v87 <74028234+debase-v87@users.noreply.github.com>
2020-11-11 13:27:45 -0600

    Content changes

--- src/info/Governance.js
+++ src/info/Governance.js
@@ -10,27 +10,26 @@ export default function Governance({ scrollToParameters, scrollToOwnership, isMo
 			<div className=" block  ">
 				<div className={isMobile ? 'box content' : 'box content is-medium'}>
 					<p>
-						Degov tokens represent a vote in the governance of the protocol. The token allows a user to
-						propose new protocol or vote on changes proposed by other users. Initially to propose a new
-						proposal a user needs to have 1% of all the Degov tokens in supply. For a proposal to pass a
-						quorum of 10% of the current Degov token supply needs to be reached.
+						DEGOV tokens represent a vote in the governance of the protocol. The token allows a user to
+						propose new protocols or vote on changes proposed by other users. Initially, for new proposals a user needs to have 1% of all 
+						the DEGOV tokens in supply. For a proposal to pass, a
+						quorum of 10% of the current DEGOV token supply needs to be reached.
 					</p>
 					<p>
-						When a proposal is passed it will be queued and can be executed after a set delay of 2 days by a
-						Timelock contract, that behaves as the owner of the editable contracts. For better detail on
-						this behavior please view how compound finance's{' '}
-						<a href="https://compound.finance/docs/governance">governance</a> works of which Degov
-						governance is based of.
+						When a proposal is passed, it will be queued and can be executed after a set delay of 2 days by a
+						Timelock contract which behaves as the owner of the editable contracts. For more details on
+						this behavior, please view how Compound Finance's{' '}
+						<a href="https://compound.finance/docs/governance">governance</a> works, as Debaseonomics' governance is forked from it.
 					</p>
 					<p>
-						So what Degov offers a user is control from how governance parameters should behave to how
-						rebasing parameters should behave. Giving users an unprecedented level of control to answer the
-						question whether the community can build a token that will eventually be stable.
+						What DEGOV offers a user is wide ranging control of governance parameters as well as 
+						rebasing parameters. This framework gives users an unprecedented level of control to find the best solutions to 
+						stabilize DEBASE (and thus, elastic supply tokens) in the long run.
 					</p>
 					<p>
-						To view the types of editable parameters please view the {/* eslint-disable-next-line */}
-						<a onClick={() => scrollToParameters()}>parameters</a> page to see what they are and there
-						current values. Also, to view the ownership transfer to the time lock contract please view the{' '}
+						For a detailed view of editable parameters and their current values please view {/* eslint-disable-next-line */}
+						<a onClick={() => scrollToParameters()}>the Parameters section</a>. 
+						To view the ownership transfer to the time lock contract, please view the{' '}
 						{/* eslint-disable-next-line */}
 						<a onClick={() => scrollToOwnership()}>ownership</a> page.
 					</p>
