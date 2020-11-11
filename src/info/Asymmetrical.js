08ae420e56bfd478de2933f5ff1a50664b0543a7
debase-v87 <74028234+debase-v87@users.noreply.github.com>
2020-11-11 12:46:49 -0600

    Update Asymmetrical.js

--- src/info/Asymmetrical.js
+++ src/info/Asymmetrical.js
@@ -10,28 +10,25 @@ export default function Asymmetrical({ isMobile }) {
 			<div className=" block  ">
 				<div className={isMobile ? 'box content' : 'box content is-medium'}>
 					<p>
-						Debase allows for the community to adjust the smoothing parameter (Rebase Lag) over configurable
-						ranges that is applied in-order to dampen supply changes during a rebase. Thus preventing the
-						protocol from increasing or decreasing the supply too fast.
+						Debaseonomics allows for the governance to adjust the smoothing parameter (Rebase Lag) 
+						over configurable ranges that is applied in-order to dampen supply changes during a rebase. 
+						The rebase lag can be configured to behave in accordance with both the amplitude 
+						and sign of deviation between the current price and the target price thus allowing for assymmetric lag.
+						This flexibility prevents the protocol from increasing or decreasing the supply too fast 
+						if suitable to circumstance. 
+
+					
 					</p>
 					<p>
-						So the rebase lag can be configured to behave differently according to how far the current price
-						is from the target price and if the current price is above or below the target price. Thus
-						allowing for asymmetrical lag parameters that are applied according to the current deviation
-						from the target price.
+						For instance, say the supply needs to increase by an addition of 1000 DEBASE tokens to account 
+						for the price difference from the target price, and governance has configured a lag parameter of 20 to be applied for the supply change range of 500-1500. 
+						Here, the supply increase will dampen by a factor of 20 and to reduce the supply increase to 50 Debase only. 
+						Thus a dramatic increase in supply will not be seen.
 					</p>
 					<p>
-						So say the supply needs to increase by an addition 1000 Debase tokens to account for the price
-						difference from the target price, and we have configured a lag parameter of 20 to applied for
-						the supply change range of 500-1500. We will dampen the supply increase by dividing the supply
-						increase by 20 and to reduce the supply change to 50 Debase only. Thus, we would not need such a
-						dramatic increase in the supply.
-					</p>
-					<p>
-						Compared to the supply change being 10000 Debase tokens we will apply a lower lag parameter of 5
-						to lessen the smoothing effect on the supply change thus in theory enforcing a more dramatic
-						price change since the number of tokens have greatly increased or decreased if the supply change
-						was negative.
+						On the other hand, if 10000 DEBASE tokens are to be added in rebase, a lower lag parameter of 5 can be used 
+						to lessen the smoothing effect on the supply change. In theory, this enforces a more dramatic supply change 
+						(and presumably, price change) relative to the previous example.
 					</p>
 				</div>
 			</div>
