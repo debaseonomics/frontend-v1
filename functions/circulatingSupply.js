6b371c5ffd115b6c4df082bd5e077b8fd75fb050
PunkUnknown <PunkUnknown@tutanota.com>
2020-11-29 11:51:04 +0500

    Added burned tokens to function

--- functions/circulatingSupply.js
+++ functions/circulatingSupply.js
@@ -9,7 +9,11 @@ exports.handler = async function(event, context) {
 
 	const totalSupply = await contract.totalSupply();
 	const stabilizerBalance = await contract.balanceOf('0x989Edd2e87B1706AB25b2E8d9D9480DE3Cc383eD');
-	const circBalance = ethers.utils.formatEther(totalSupply.sub(stabilizerBalance));
+	const pool1Balance = await contract.balanceOf('0xf5cB771023706Ca566eA6128b88e03A262737479');
+	const pool2Balance = await contract.balanceOf('0xF4168cc431e9a8310e595dB9F7E2564cC96F5D51');
+	const circBalance = ethers.utils.formatEther(
+		totalSupply.sub(stabilizerBalance).sub(pool1Balance).sub(pool2Balance)
+	);
 
 	return {
 		statusCode: 200,
