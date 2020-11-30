afa262f458deb432ecec20e81c505eea556ea884
PunkUnknown <PunkUnknown@tutanota.com>
2020-11-30 13:47:44 +0500

    switched to query parameters

--- functions/randomNumber.js
+++ functions/randomNumber.js
@@ -2,7 +2,8 @@ const Chance = require('chance');
 var chance = new Chance();
 
 exports.handler = async function(event, context) {
-	const num = Math.floor(chance.normal({ mean: 10, dev: 4 }));
+	const params = event.queryStringParameters;
+	let num = Math.floor(chance.normal({ mean: parseInt(params.mean), dev: parseInt(params.sigma) }));
 	if (num < 0) {
 		num = 0;
 	}
