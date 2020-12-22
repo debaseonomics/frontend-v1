dd763e5e7eece217ca419bb8ec02f52011a7a5f9
smallinvestor <jumpupdnbzone@gmail.com>
2020-12-22 15:50:48 +0100

    reward % fix

--- src/dapp/stabilizers/ThresholdCounter.js
+++ src/dapp/stabilizers/ThresholdCounter.js
@@ -63,14 +63,14 @@ export default function ThresholdCounter() {
 		fetcher: fetcher(library, randomNumberAbi)
 	});
 
-	const { data: randomThreshold } = useSWR([contractAddress.stabilizerPool, 'normalDistribution', randomNumber ? parseInt(formatEther(randomNumber)) % 100 : 0 ], {
+	const { data: randomThreshold } = useSWR([contractAddress.stabilizerPool, 'normalDistribution', randomNumber ? parseInt(formatEther(randomNumber)) % 100 : 0], {
 		fetcher: fetcher(library, thresholdCounterAbi)
 	});
 
 	const paramsData = [
 		{
 			label: 'Reward',
-			value: rewardPercentage ? formatEther(rewardPercentage) + '%' : '...',
+			value: rewardPercentage ? formatEther(rewardPercentage) * 100 + '%' : '...',
 			toolTip: 'Percentage of stabilizer rewards contract requested as reward per reward duration'
 		},
 		{
@@ -120,12 +120,12 @@ export default function ThresholdCounter() {
 		},
 		{
 			label: 'Current Count',
-			value: count ? parseInt(formatUnits(count,0)) : '...',
+			value: count ? parseInt(formatUnits(count, 0)) : '...',
 			toolTip: 'Percentage of rewards that will be revoked if positive rebases stop'
 		},
 		{
 			label: 'Last Random Threshold',
-			value: randomThreshold ? parseInt(formatUnits(randomThreshold,0))  : '...',
+			value: randomThreshold ? parseInt(formatUnits(randomThreshold, 0)) : '...',
 			toolTip: 'Percentage of rewards that will be revoked if positive rebases stop'
 		}
 	];
@@ -154,28 +154,30 @@ export default function ThresholdCounter() {
 								This stabilizer counts the number of positive rebases until a random thresholdis hit, sampled from a normal distribution. Once the threshold is hit, counter is reset and the pool starts to reward DEBASE for staked DEBASE/DAI LPs, as per the parameters mentioned.
 							</h5>
 							<span className="is-inline subtitle is-size-5-tablet is-size-6-mobile mb-0">
-								<svg
-									width="16"
-									height="16"
-									viewBox="0 0 24 24"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path d="M7 18H17V16H7V18Z" fill="currentColor" />
-									<path d="M17 14H7V12H17V14Z" fill="currentColor" />
-									<path d="M7 10H11V8H7V10Z" fill="currentColor" />
-									<path
-										fillRule="evenodd"
-										clipRule="evenodd"
-										d="M6 2C4.34315 2 3 3.34315 3 5V19C3 20.6569 4.34315 22 6 22H18C19.6569 22 21 20.6569 21 19V9C21 5.13401 17.866 2 14 2H6ZM6 4H13V9H19V19C19 19.5523 18.5523 20 18 20H6C5.44772 20 5 19.5523 5 19V5C5 4.44772 5.44772 4 6 4ZM15 4.10002C16.6113 4.4271 17.9413 5.52906 18.584 7H15V4.10002Z"
-										fill="currentColor"
-									/>
-								</svg>
+
 								<a
+									className="is-primary"
 									target="_blank"
 									rel="noopener noreferrer"
 									href={etherScanAddress + contractAddress.stabilizerPool}
 								>
+									<svg
+										width="16"
+										height="16"
+										viewBox="0 0 24 24"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path d="M7 18H17V16H7V18Z" fill="currentColor" />
+										<path d="M17 14H7V12H17V14Z" fill="currentColor" />
+										<path d="M7 10H11V8H7V10Z" fill="currentColor" />
+										<path
+											fillRule="evenodd"
+											clipRule="evenodd"
+											d="M6 2C4.34315 2 3 3.34315 3 5V19C3 20.6569 4.34315 22 6 22H18C19.6569 22 21 20.6569 21 19V9C21 5.13401 17.866 2 14 2H6ZM6 4H13V9H19V19C19 19.5523 18.5523 20 18 20H6C5.44772 20 5 19.5523 5 19V5C5 4.44772 5.44772 4 6 4ZM15 4.10002C16.6113 4.4271 17.9413 5.52906 18.584 7H15V4.10002Z"
+											fill="currentColor"
+										/>
+									</svg>
 									{turncate(contractAddress.stabilizerPool, 18, '...')}
 								</a>
 							</span>
